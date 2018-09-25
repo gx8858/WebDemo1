785701ced570152149f62e0e259599bcbe65e7e4
GaoXiang <39550837+gx8858@users.noreply.github.com>
2018-09-25 22:18:50 +0800

    referer 记住当前网页的来源（防止盗链）
    req.getHeader("referer")

--- src/cn/itcast/request/HeaderServlet.java
+++ src/cn/itcast/request/HeaderServlet.java
@@ -12,8 +12,29 @@ public class HeaderServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String agent = req.getHeader("user-agent");
-		System.out.println(agent);
+		// 判断浏览器的版本
+//		String agent = req.getHeader("user-agent");
+//		System.out.println(agent);
+				
+		// 先通过referer头获取内容
+		String referer = req.getHeader("referer");
+		// System.out.println(referer);
+		// http://localhost:8080/test/my/index.html
+		// http://localhost:8080/test/bad/index.html
+		
+		/*response.setHeader("Content-Type", "text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");*/
+		
+		// 字符中文乱码的问题
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		if(referer != null && referer.startsWith("http://localhost:8080/test/my")){
+			// 正常播放
+			resp.getWriter().write("电影已经播放了...");
+		}else{
+			// 提示
+			resp.getWriter().write("呸，臭不要脸的...");
+		}
 	}
 
 	@Override
